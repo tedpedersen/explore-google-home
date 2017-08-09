6c1237fff2c6290ccd7a9743438cc450d4f8e914
Ted Pedersen <tedpedersen@me.com>
2017-08-09 14:51:40 -0700

    google stuff

--- server.js
+++ server.js
@@ -6,20 +6,24 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var routes = require('./api/routes/apiroutes');
 
+require('@google-cloud/debug-agent').start();
 
+const express = require('express');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);
 
-//Express configuration 
+//Express configuration
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(options, app);
 
-//Sample URL
-//HTTPS : https://10.149.251.186:8443/tasks
-//HTTP : http://10.149.251.186:8080/tasks
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
-console.log("Listening in following ports 8080, 8443");
+// Start the server
+if (module === require.main) {
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+  });
+}
